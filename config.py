f75d86c4f8a88698b99b63d2635b4d67c63ed6ac
eric <yogesh1singh1@gmail.com>
2026-07-13 07:58:14 +0530

    Add project config file

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+class Config:
+    SECRET_KEY = "your-secret-key"
+
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    SQLALCHEMY_DATABASE_URI = (
+        "sqlite:///" + os.path.join(BASE_DIR, "database", "hrms.db")
+    )
+
+    SQLALCHEMY_TRACK_MODIFICATION = False
